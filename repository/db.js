ec942777ea6c44878f588fea35a44d7817cb4514
dahee <kimdahee42@gmail.com>
2025-04-24 18:41:45 +0900

    commit

--- repository/db.js
+++ repository/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'jinair-project.s3-website.ap-northeast-2.amazonaws.com/', 
+    host: 'shoppy-redux-db.cjesyyk6c1ln.ap-northeast-2.rds.amazonaws.com', 
     port: 3306,
     user: 'admin',
     password: 'shoppy-admin-1234',
